85dc5ad1622b9a017597047ebe1a4c5480f55c89
Sheff3rd <thedoctorwhoovesmail@gmail.com>
2016-11-06 11:16:32 +0200

    Paginate fixes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,8 +4,7 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = Task.all.order(:priority)
-    # Task.paginate(:page => params[:page], :per_page => 3)
+    @tasks = Task.all.order(:priority).paginate(page: params[:page], per_page: 3)
   end
 
   def open
